a41bd9401342b0c0deca18c8025ef15bedbc43e7
Naman Singh Rana <nsr@Namans-MacBook-Air.local>
2023-03-23 12:09:43 +0530

    modified termination condition

--- ass2/handler.py
+++ ass2/handler.py
@@ -1,7 +1,7 @@
 from event_queue import event_queue
 from network import Network
 from params import *
-
+from block import Block
 class Handler:
     """initializes a network and runs the simulation
     """
@@ -9,15 +9,21 @@ class Handler:
         """initializes a network
         """
         self.network = Network(n,z0,z1)
-        self.iterations = ITERATIONS
+        self.blk_count = BLK_THRESHOLD
     def run(self):
         """function to run the simulation
         """
-        for i in range(self.iterations):
+        while Block.available_id < self.blk_count:
             event = event_queue.get_next_event()
             if event is None:
                 break
             self.network.process_event(event)
+        while True:
+            event = event_queue.get_next_event()
+            if event.event_type == GENERATE_BLK:
+                break
+            self.network.process_event(event)
+            
         self.analyze()
     def analyze(self):
         """function to analyze the simulation
