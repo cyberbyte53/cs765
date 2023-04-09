556de3b07bc6c7d47c4325678afc7301b3c165e7
Naman Singh Rana <nsr@Namans-MacBook-Air.local>
2023-04-09 13:01:58 +0530

    minor bug fixes

--- ass3/migrations/2_deploy_contracts.js
+++ ass3/migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var Payment = artifacts.require("/Users/nsr/765/ass3/contracts/Payment.sol");
+var Payment = artifacts.require("../contracts/Payment.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Payment);
