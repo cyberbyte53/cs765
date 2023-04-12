9fb977283a4a48d24b9fbe5fb4bb0063c6a4fd6b
Naman Singh Rana <nsr@Namans-MacBook-Air.local>
2023-04-12 21:23:01 +0530

    ass3 final submission

--- ass3/params.py
+++ ass3/params.py
@@ -1,7 +1,7 @@
 n = 100  # number of nodes
 m = 1  # number of random edges to add for each new node
 p = 0.5  # probability of adding a triangle after adding a random edge
-mean_starting_balance = 10
+mean_starting_balance = 100
 num_transactions = 1000
 print_freq = num_transactions/10
 debug=False
